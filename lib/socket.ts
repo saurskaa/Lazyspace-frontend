3b0c9f9b0b83af3c32970b74a68e8ba6d185237b
saurskaa <saurska321@gmail.com>
2026-03-02 22:38:56 +0530

    removed comment

--- lib/socket.ts
+++ lib/socket.ts
@@ -10,7 +10,7 @@ const pendingMessages: any[] = [];
 
 const WS_BASE_URL =
   process.env.NEXT_PUBLIC_WS_URL || AppConst.NEXT_PUBLIC_WS_URL;
-// process.env.NEXT_PUBLIC_WS_URL || "ws://10.105.152.199:3000";
+
 
 let reconnectAttempts = 0;
 let reconnectTimer: NodeJS.Timeout | null = null;
